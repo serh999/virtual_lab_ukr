784395b1f3b1b522c62df45776580eed1c064c8a
SergeyD <131809998+serh999@users.noreply.github.com>
2025-01-02 21:39:48 +0200

    Update reactions.js

--- school/8class/lab8_3/properties/reactions.js
+++ school/8class/lab8_3/properties/reactions.js
@@ -3,6 +3,13 @@ var reactions = {
         "REACTION": [
             {
                 "SPECIES_REF": [
+                    { "id": 0, "coefficient": -1 }, // H2O
+                    { "id": 1, "coefficient": 1 }, // H +
+                    { "id": 2, "coefficient": 1 } // OH -
+                ]
+            },
+            {
+                "SPECIES_REF": [
                     { "id": 5, "coefficient": -1 }, // Ag +
                     { "id": 3, "coefficient": -1 }, // Cl -
                     { "id": 7, "coefficient": 1 } // AgCl
