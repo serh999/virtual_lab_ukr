d3519369c6ae3d169b05e158b561e98b8f21cc07
SergeyD <131809998+serh999@users.noreply.github.com>
2025-01-06 23:40:09 +0200

    Add files via upload

---
+++ school/8class/lab8_7/properties/solutions.js
@@ -0,0 +1,25 @@
+var solutions = {
+    "FILESYSTEM": {
+        "DIRECTORY": [
+            {
+                "name": "stockroom",
+                "SOLUTION": [
+                    {
+                        "name": "Cu(OH)<sub>2</sub>",
+                        "description": "Купрум(II) гідроксид",
+                        "volume": "0.1",
+                        "species": [
+                          {
+                            "id": 0
+                          },
+                          {
+                            "id": 3,
+                            "amount": 0.00102501
+                          }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
---
+++ school/8class/lab8_7/properties/species.js
@@ -0,0 +1,54 @@
+var species = {
+    "SPECIES_LIST": {
+        "SPECIES": [
+            {
+                "id": 0,
+                "name": "H<sub>2</sub>O",
+                "enthalpy": -285.83,
+                "entropy": 69.91,
+                "state": "l",
+                "molecularWeight": 18.016
+            },
+            {
+                "id": 1,
+                "name": "H<sup>+</sup>",
+                "enthalpy": 0.0,
+                "entropy": 0.0,
+                "molecularWeight": 1.008
+            },
+            {
+                "id": 2,
+                "name": "OH<sup>-</sup>",
+                "enthalpy": -229.99,
+                "entropy": -10.75,
+                "molecularWeight": 17.008
+            },
+            {
+                "id": 3,
+                "name": "Cu(OH)<sub>2</sub>",
+                "enthalpy": -387.6309,
+                "entropy": 90.6,
+                "state": "s",
+                "hue": 240,
+                "saturation": 100,
+                "value": 100,
+                "colorConcentration": 0.1,
+                "density": 0.3,
+                "molecularWeight": 97.56
+            },
+            {
+                "id": 4,
+                "name": "CuO",
+                "enthalpy": -95,
+                "entropy": 43.5,
+                "state": "s",
+                "hue": 0,
+                "saturation": 0,
+                "value": 0,
+                "colorConcentration": 1.0,
+                "density": 6.31,
+                "molecularWeight": 79.55
+            }
+        ]
+    }
+}
