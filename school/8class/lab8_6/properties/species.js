46fdb91ec0e40dabdafcd04baa9485f00a28078b
SergeyD <131809998+serh999@users.noreply.github.com>
2025-01-06 20:08:53 +0200

    Add files via upload

---
+++ school/8class/lab8_6/properties/species.js
@@ -0,0 +1,66 @@
+var species = {
+    "SPECIES_LIST": {
+        "SPECIES": [
+            {
+                "id": 0,
+                "name": "H<sub>2</sub>O",
+                "enthalpy": -285.83,
+                "entropy": 69.91,
+                "state": "l",
+                "molecularWeight": 18.016
+            },
+            {
+                "id": 1,
+                "name": "H<sup>+</sup>",
+                "enthalpy": 0.0,
+                "entropy": 0.0,
+                "molecularWeight": 1.008
+            },
+            {
+                "id": 2,
+                "name": "OH<sup>-</sup>",
+                "enthalpy": -229.99,
+                "entropy": -10.75,
+                "molecularWeight": 17.008
+            },
+            {
+                "id": 3,
+                "name": "Na<sup>+</sup>",
+                "enthalpy": -240.12,
+                "entropy": 59.0,
+                "molecularWeight": 22.99
+            },
+            {
+                "id": 4,
+                "name": "Cu<sup>2+</sup>",
+                "enthalpy": 64.77,
+                "entropy": -99.6,
+                "hue": 200,
+                "saturation": 60,
+                "value": 90,
+                "colorConcentration": 0.03,
+                "molecularWeight": 63.55
+            },
+            {
+                "id": 5,
+                "name": "SO<sub>4</sub><sup>2-</sup>",
+                "enthalpy": -907.51,
+                "entropy": 17.15,
+                "molecularWeight": 96.07
+            },
+            {
+                "id": 6,
+                "name": "Cu(OH)<sub>2</sub>",
+                "enthalpy": -555.0,
+                "entropy": -387.8934,
+                "state": "s",
+                "hue": 240,
+                "saturation": 100,
+                "value": 100,
+                "colorConcentration": 0.1,
+                "density": 0.3,
+                "molecularWeight": 97.56
+            }
+        ]
+    }
+}
